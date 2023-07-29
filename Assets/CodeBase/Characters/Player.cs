3a5d914cb4af6ec9776a0b8ed10d5538e190cda9
Egor Shtan'ko <sasykeuhiha@gmail.com>
2023-07-29 16:35:01 +0300

    collision detection

--- Assets/CodeBase/Characters/Player.cs
+++ Assets/CodeBase/Characters/Player.cs
@@ -13,16 +13,39 @@ namespace CodeBase.Characters
         {
             var inputVector = inputHandler.GetMovementVectorNormalized();
             var direction = new Vector3(inputVector.x, 0f, inputVector.y);
+            var acceleration = Time.deltaTime * data.speed;
+            var canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * data.playerHeight, data.playerRadius, direction, acceleration);
 
             if (IsNotMoving(direction))
             {
                 animator.StopMoving();
                 return;
             }
-
-            transform.position += (direction * (Time.deltaTime * data.speed));
-            transform.forward = Vector3.Slerp(transform.forward, direction, Time.deltaTime * data.rotationSpeed);
             
+            if (!canMove)
+            {
+                var directionX = new Vector3(direction.x, 0, 0).normalized;
+                var canMoveX = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * data.playerHeight, data.playerRadius, directionX, acceleration);
+
+                if (canMoveX)
+                    direction = directionX;
+                else
+                {
+                    var directionZ = new Vector3(0, 0, direction.z).normalized;
+                    var canMoveZ = !Physics.CapsuleCast(transform.position,
+                        transform.position + Vector3.up * data.playerHeight, data.playerRadius, directionZ,
+                        acceleration);
+
+                    if (canMoveZ)
+                        direction = directionZ;
+                    else
+                        return;
+                }
+
+            }
+
+            transform.position += direction * acceleration;
+            transform.forward = Vector3.Slerp(transform.forward, direction, Time.deltaTime * data.rotationSpeed);
             animator.StartMoving();
         }
 
--- Assets/CodeBase/StaticData/PlayerStaticData.cs
+++ Assets/CodeBase/StaticData/PlayerStaticData.cs
@@ -5,6 +5,8 @@ namespace CodeBase.StaticData
     [CreateAssetMenu(menuName = "StaticData/Player", fileName = "PlayerStaticData", order = 0)]
     public class PlayerStaticData : ScriptableObject
     {
+        public float playerRadius = .7f;
+        public float playerHeight = 2f;
         public float speed;
         public float rotationSpeed;
     }
