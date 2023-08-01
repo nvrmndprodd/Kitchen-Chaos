015b62c6f46116ee3a4500c4edb75d5c02a7b28c
Egor Shtan'ko <sasykeuhiha@gmail.com>
2023-08-01 14:44:26 +0300

    kitchen objects

---
+++ Assets/CodeBase/KitchenObject.cs
@@ -0,0 +1,12 @@
+﻿using CodeBase.StaticData;
+using UnityEngine;
+
+namespace CodeBase
+{
+    public class KitchenObject : MonoBehaviour
+    {
+        [SerializeField] private KitchenObjectStaticData kitchenObjectData;
+
+        public ClearCounter clearCounter;
+    }
+}
---
+++ Assets/CodeBase/StaticData/KitchenObjectStaticData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace CodeBase.StaticData
+{
+    [CreateAssetMenu(menuName = "StaticData/KitchenObject", fileName = "KitchenObjectStaticData")]
+    public class KitchenObjectStaticData : ScriptableObject
+    {
+        public KitchenObject prefab;
+        public Sprite icon;
+        public string objectName;
+    }
+}
