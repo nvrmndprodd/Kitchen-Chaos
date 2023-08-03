4d04972bbe9c70b991ffa16c75784f10f66d533e
Egor Shtan'ko <sasykeuhiha@gmail.com>
2023-08-03 13:12:00 +0300

    i mean... this is stove counter

---
+++ Assets/CodeBase/Counters/TrashCounter/TrashCounter.cs
@@ -0,0 +1,13 @@
+﻿using CodeBase.KitchenObject;
+
+namespace CodeBase.Counters.TrashCounter
+{
+    public class TrashCounter : BaseCounter
+    {
+        public override void Interact(IKitchenObjectParent newParent)
+        {
+            if (newParent.HasKitchenObject) 
+                newParent.KitchenObject.DestroySelf();
+        }
+    }
+}
