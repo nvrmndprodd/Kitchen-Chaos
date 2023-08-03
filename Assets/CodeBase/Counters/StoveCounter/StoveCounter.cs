0a63b940b91a83f5e6428f946ce20494242f72e7
Egor Shtan'ko <sasykeuhiha@gmail.com>
2023-08-03 13:03:38 +0300

    trash counter

---
+++ Assets/CodeBase/Counters/StoveCounter/StoveCounter.cs
@@ -0,0 +1,12 @@
+﻿using CodeBase.KitchenObject;
+
+namespace CodeBase.Counters.StoveCounter
+{
+    public class StoveCounter : BaseCounter
+    {
+        public override void Interact(IKitchenObjectParent newParent)
+        {
+            
+        }
+    }
+}
